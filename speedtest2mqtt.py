0775b7b703d596b303fb120e8d1a813c247b96ea
Oliver Wieland <oli@team-wieland.de>
2021-12-29 09:26:18 +0100

    introduced logging

--- speedtest2mqtt.py
+++ speedtest2mqtt.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import logging
 #import json
 import paho.mqtt.client as mqtt
 
@@ -8,7 +9,7 @@ try:
 except KeyError:
     MQTT_HOST = 'localhost'
 try:
-MQTT_PORT = os.environ['MQTT_PORT']
+    MQTT_PORT = os.environ['MQTT_PORT']
 except KeyError:
     MQTT_PORT = 1883
 try:
@@ -17,29 +18,38 @@ except KeyError:
     MQTT_TOPIC = 'speedtest'
 
 def call_speedtest():
+    logging.info("Performing speedtest")
     speedtest = subprocess.Popen(["./speedtest", "--accept-license", "--accept-gdpr", "-fjson"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = speedtest.communicate()
     out = out.decode("utf8")
     err = err.decode("utf8")
 
     if out == "":
+        logging.info("Speedtest failed with error")
         out = "\"\""
         err = "true"
     else:
+        logging.info("Speedtest ok")
         err = "false"
     client.publish(MQTT_TOPIC, "{\"out\":%s, \"err\":\"%s\"}" % (out, err))
 
 
 def on_connect(client, obj, flacs, rc):
+    logging.info("MQTT connected")
     client.publish(MQTT_TOPIC + '/online', True, qos=1, retain=True)
     
 def on_message(client, userdata, message):
+    logging.info("MQTT message received")
+
     topic = message.topic
     payload = message.payload.decode("utf-8")
+    logging.debug("Topic: %s\nPayload: %s",  topic, payload)
     if topic == "%s/cmd" % MQTT_TOPIC:
         if payload == "true":
             call_speedtest()
 
+logging.basicConfig(level=logging.INFO)
+logging.info("Starting speedtest2mqtt")
 client = mqtt.Client("speedtest")
 client.on_connect=on_connect
 client.on_message=on_message
